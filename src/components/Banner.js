bbfb551ea00355abfb83cbef0812bcb0fb9a9a62
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2025-02-17 08:59:14 +0000

    guide

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,11 +1,11 @@
-import logo from '../assets/logo.png'
-import '../styles/Banner.css'
+import logo from '../assets/logo.png' //  chemin vers le logo
+import '../styles/Banner.css'  // importation du fichier CSS
 
 function Banner() {
     const title = 'La maison jungle'
     return (
         <div className='lmj-banner'>
-            <img src={logo} alt='La maison jungle' className='lmj-logo' />
+            <img src={logo} alt='La maison jungle' className='lmj-logo' /> 
             <h1 className='lmj-title'>{title}</h1>
         </div>
     )
